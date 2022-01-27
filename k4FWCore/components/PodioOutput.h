775600c128abd126da67f70f1a9da182ae198111
Placido Fernandez Declara <placido.fernandez@cern.ch>
2022-01-27 15:29:26 +0100

    Use podio ROOTWriter to write events

--- k4FWCore/components/PodioOutput.h
+++ k4FWCore/components/PodioOutput.h
@@ -1,16 +1,12 @@
 #ifndef FWCORE_PODIOOUTPUT_H
 #define FWCORE_PODIOOUTPUT_H
 
-#include "k4FWCore/KeepDropSwitch.h"
 #include "GaudiAlg/GaudiAlgorithm.h"
-#include "podio/CollectionBase.h"
-
-#include "TTree.h"
+#include "podio/ROOTWriter.h"
 
 #include <vector>
 
 // forward declarations
-class TFile;
 class PodioDataSvc;
 
 class PodioOutput : public GaudiAlgorithm {
@@ -19,42 +15,18 @@ public:
   /// Constructor.
   PodioOutput(const std::string& name, ISvcLocator* svcLoc);
 
-  /// Initialization of PodioOutput. Acquires the data service, creates trees and root file.
+  /// Initialization of PodioOutput. Acquires the data service, creates ROOTWriter from podio
   virtual StatusCode initialize();
-  /// Execute. For the first event creates branches for all collections known to PodioDataSvc and prepares them for
-  /// writing. For the following events it reconnects the branches with collections and prepares them for write.
   virtual StatusCode execute();
-  /// Finalize. Writes the meta data tree; writes file and cleans up all ROOT-pointers.
   virtual StatusCode finalize();
 
 private:
-  void resetBranches(const std::vector<std::pair<std::string, podio::CollectionBase*>>& collections);
-  void createBranches(const std::vector<std::pair<std::string, podio::CollectionBase*>>& collections);
-  /// First event or not
-  bool m_firstEvent;
   /// Root file name the output is written to
   Gaudi::Property<std::string> m_filename{this, "filename", "output.root", "Name of the file to create"};
-  /// Commands which output is to be kept
-  Gaudi::Property<std::vector<std::string>> m_outputCommands{
-      this, "outputCommands", {"keep *"}, "A set of commands to declare which collections to keep or drop."};
-  Gaudi::Property<std::string> m_filenameRemote{
-      this, "remoteFilename", "", "An optional file path to copy the outputfile to."};
-  /// Switch for keeping or dropping outputs
-  KeepDropSwitch m_switch;
   /// Needed for collection ID table
   PodioDataSvc* m_podioDataSvc;
-  /// The actual ROOT file
-  std::unique_ptr<TFile> m_file;
-  /// The tree to be filled with collections
-  TTree* m_datatree;
-  /// The tree to be filled with meta data
-  TTree* m_metadatatree;
-  TTree* m_runMDtree;
-  TTree* m_evtMDtree;
-  TTree* m_colMDtree;
-
-  /// The stored collections
-  std::vector<podio::CollectionBase*> m_storedCollections;
+  // ROOT writer to persist events into .root files
+  std::unique_ptr<podio::ROOTWriter> m_ROOTWriter;
 };
 
 #endif
